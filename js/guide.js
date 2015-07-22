93288d5a406124d2d4df7cd8b4e917e13172aab2
kmin <kmin9102@gmail.com>
2015-07-23 01:33:22 +0900

    오른쪽 챕터선택+3

--- js/guide.js
+++ js/guide.js
@@ -4,15 +4,16 @@
 
 function selChap() {
     // Use the identity matrix while clearing the canvas
+
     var ch =  $('#selChapter').val();
 
-    if(ch == 'ch1') {
-        var a = document.getElementById('ch1');
-        var b = document.getElementById('ch2');
-        var c = document.getElementById('ch3');
-        var d = document.getElementById('ch4');
-        var e = document.getElementById('ch5');
-        var f = document.getElementById('ch6');
+    if(ch == 'ch2') {
+        var a = document.getElementById('ch2');
+        var b = document.getElementById('ch3');
+        var c = document.getElementById('ch4');
+        var d = document.getElementById('ch5');
+        var e = document.getElementById('ch6');
+        var f = document.getElementById('ch7');
         a.style.display = "block";
         b.style.display = "none";
         c.style.display = "none";
@@ -20,13 +21,13 @@ function selChap() {
         e.style.display = "none";
         f.style.display = "none";
     }
-    else if(ch == 'ch2') {
-        var a = document.getElementById('ch1');
-        var b = document.getElementById('ch2');
-        var c = document.getElementById('ch3');
-        var d = document.getElementById('ch4');
-        var e = document.getElementById('ch5');
-        var f = document.getElementById('ch6');
+    else if(ch == 'ch3') {
+        var a = document.getElementById('ch2');
+        var b = document.getElementById('ch3');
+        var c = document.getElementById('ch4');
+        var d = document.getElementById('ch5');
+        var e = document.getElementById('ch6');
+        var f = document.getElementById('ch7');
         a.style.display = "none";
         b.style.display = "block";
         c.style.display = "none";
@@ -34,13 +35,13 @@ function selChap() {
         e.style.display = "none";
         f.style.display = "none";
     }
-    else if(ch == 'ch3') {
-        var a = document.getElementById('ch1');
-        var b = document.getElementById('ch2');
-        var c = document.getElementById('ch3');
-        var d = document.getElementById('ch4');
-        var e = document.getElementById('ch5');
-        var f = document.getElementById('ch6');
+    else if(ch == 'ch4') {
+        var a = document.getElementById('ch2');
+        var b = document.getElementById('ch3');
+        var c = document.getElementById('ch4');
+        var d = document.getElementById('ch5');
+        var e = document.getElementById('ch6');
+        var f = document.getElementById('ch7');
         a.style.display = "none";
         b.style.display = "none";
         c.style.display = "block";
@@ -48,13 +49,13 @@ function selChap() {
         e.style.display = "none";
         f.style.display = "none";
     }
-    else if(ch == 'ch4') {
-        var a = document.getElementById('ch1');
-        var b = document.getElementById('ch2');
-        var c = document.getElementById('ch3');
-        var d = document.getElementById('ch4');
-        var e = document.getElementById('ch5');
-        var f = document.getElementById('ch6');
+    else if(ch == 'ch5') {
+        var a = document.getElementById('ch2');
+        var b = document.getElementById('ch3');
+        var c = document.getElementById('ch4');
+        var d = document.getElementById('ch5');
+        var e = document.getElementById('ch6');
+        var f = document.getElementById('ch7');
         a.style.display = "none";
         b.style.display = "none";
         c.style.display = "none";
@@ -62,13 +63,13 @@ function selChap() {
         e.style.display = "none";
         f.style.display = "none";
     }
-    else if(ch == 'ch5') {
-        var a = document.getElementById('ch1');
-        var b = document.getElementById('ch2');
-        var c = document.getElementById('ch3');
-        var d = document.getElementById('ch4');
-        var e = document.getElementById('ch5');
-        var f = document.getElementById('ch6');
+    else if(ch == 'ch6') {
+        var a = document.getElementById('ch2');
+        var b = document.getElementById('ch3');
+        var c = document.getElementById('ch4');
+        var d = document.getElementById('ch5');
+        var e = document.getElementById('ch6');
+        var f = document.getElementById('ch7');
         a.style.display = "none";
         b.style.display = "none";
         c.style.display = "none";
@@ -76,13 +77,13 @@ function selChap() {
         e.style.display = "block";
         f.style.display = "none";
     }
-    else if(ch == 'ch6') {
-        var a = document.getElementById('ch1');
-        var b = document.getElementById('ch2');
-        var c = document.getElementById('ch3');
-        var d = document.getElementById('ch4');
-        var e = document.getElementById('ch5');
-        var f = document.getElementById('ch6');
+    else if(ch == 'ch7') {
+        var a = document.getElementById('ch2');
+        var b = document.getElementById('ch3');
+        var c = document.getElementById('ch4');
+        var d = document.getElementById('ch5');
+        var e = document.getElementById('ch6');
+        var f = document.getElementById('ch7');
         a.style.display = "none";
         b.style.display = "none";
         c.style.display = "none";
@@ -91,106 +92,18 @@ function selChap() {
         f.style.display = "block";
     }
     else {
-        var a = document.getElementById('ch1');
-        var b = document.getElementById('ch2');
-        var c = document.getElementById('ch3');
-        var d = document.getElementById('ch4');
-        var e = document.getElementById('ch5');
-        var f = document.getElementById('ch6');
+        var a = document.getElementById('ch2');
+        var b = document.getElementById('ch3');
+        var c = document.getElementById('ch4');
+        var d = document.getElementById('ch5');
+        var e = document.getElementById('ch6');
+        var f = document.getElementById('ch7');
         a.style.display = "none";
         b.style.display = "none";
         c.style.display = "none";
         d.style.display = "none";
         e.style.display = "none";
         f.style.display = "none";
-    }
-}
-
-function show1() {
-    // Use the identity matrix while clearing the canvas
-    var qn =  $('#ch1').val();
-    if(qn == 'q1') {
-        var a = document.getElementById('1_1');
-        var b = document.getElementById('1_2');
-        var c = document.getElementById('1_3');
-        var d = document.getElementById('1_4');
-        var e = document.getElementById('1_5');
-
-        a.style.display = "block";
-        b.style.display = "none";
-        c.style.display = "none";
-        d.style.display = "none";
-        e.style.display = "none";
-    }
-
-    else if(qn == 'q2') {
-        var a = document.getElementById('1_1');
-        var b = document.getElementById('1_2');
-        var c = document.getElementById('1_3');
-        var d = document.getElementById('1_4');
-        var e = document.getElementById('1_5');
-
-        a.style.display = "none";
-        b.style.display = "block";
-        c.style.display = "none";
-        d.style.display = "none";
-        e.style.display = "none";
-    }
-
-    else if(qn == 'q3') {
-        var a = document.getElementById('1_1');
-        var b = document.getElementById('1_2');
-        var c = document.getElementById('1_3');
-        var d = document.getElementById('1_4');
-        var e = document.getElementById('1_5');
-
-        a.style.display = "none";
-        b.style.display = "none";
-        c.style.display = "block";
-        d.style.display = "none";
-        e.style.display = "none";
-    }
-
-    else if(qn == 'q4') {
-        var a = document.getElementById('1_1');
-        var b = document.getElementById('1_2');
-        var c = document.getElementById('1_3');
-        var d = document.getElementById('1_4');
-        var e = document.getElementById('1_5');
-
-        a.style.display = "none";
-        b.style.display = "none";
-        c.style.display = "none";
-        d.style.display = "block";
-        e.style.display = "none";
-    }
-
-    else if(qn == 'q5') {
-        var a = document.getElementById('1_1');
-        var b = document.getElementById('1_2');
-        var c = document.getElementById('1_3');
-        var d = document.getElementById('1_4');
-        var e = document.getElementById('1_5');
-
-        a.style.display = "none";
-        b.style.display = "none";
-        c.style.display = "none";
-        d.style.display = "none";
-        e.style.display = "block";
-    }
-
-    else {
-        var a = document.getElementById('1_1');
-        var b = document.getElementById('1_2');
-        var c = document.getElementById('1_3');
-        var d = document.getElementById('1_4');
-        var e = document.getElementById('1_5');
-
-        a.style.display = "none";
-        b.style.display = "none";
-        c.style.display = "none";
-        d.style.display = "none";
-        e.style.display = "none";
     }
 }
 
@@ -203,6 +116,122 @@ function show2() {
         var c = document.getElementById('2_3');
         var d = document.getElementById('2_4');
         var e = document.getElementById('2_5');
+        var f = document.getElementById('2_6');
+
+        a.style.display = "block";
+        b.style.display = "none";
+        c.style.display = "none";
+        d.style.display = "none";
+        e.style.display = "none";
+        f.style.display = "none";
+    }
+
+    else if(qn == 'q2') {
+        var a = document.getElementById('2_1');
+        var b = document.getElementById('2_2');
+        var c = document.getElementById('2_3');
+        var d = document.getElementById('2_4');
+        var e = document.getElementById('2_5');
+        var f = document.getElementById('2_6');
+
+        a.style.display = "none";
+        b.style.display = "block";
+        c.style.display = "none";
+        d.style.display = "none";
+        e.style.display = "none";
+        f.style.display = "none";
+    }
+
+    else if(qn == 'q3') {
+        var a = document.getElementById('2_1');
+        var b = document.getElementById('2_2');
+        var c = document.getElementById('2_3');
+        var d = document.getElementById('2_4');
+        var e = document.getElementById('2_5');
+        var f = document.getElementById('2_6');
+
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "block";
+        d.style.display = "none";
+        e.style.display = "none";
+        f.style.display = "none";
+    }
+
+    else if(qn == 'q4') {
+        var a = document.getElementById('2_1');
+        var b = document.getElementById('2_2');
+        var c = document.getElementById('2_3');
+        var d = document.getElementById('2_4');
+        var e = document.getElementById('2_5');
+        var f = document.getElementById('2_6');
+
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        d.style.display = "block";
+        e.style.display = "none";
+        f.style.display = "none";
+    }
+
+    else if(qn == 'q5') {
+        var a = document.getElementById('2_1');
+        var b = document.getElementById('2_2');
+        var c = document.getElementById('2_3');
+        var d = document.getElementById('2_4');
+        var e = document.getElementById('2_5');
+        var f = document.getElementById('2_6');
+
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        d.style.display = "none";
+        e.style.display = "block";
+        f.style.display = "none";
+    }
+
+    else if(qn == 'q6') {
+        var a = document.getElementById('2_1');
+        var b = document.getElementById('2_2');
+        var c = document.getElementById('2_3');
+        var d = document.getElementById('2_4');
+        var e = document.getElementById('2_5');
+        var f = document.getElementById('2_6');
+
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        d.style.display = "none";
+        e.style.display = "none";
+        f.style.display = "block";
+    }
+
+    else {
+        var a = document.getElementById('2_1');
+        var b = document.getElementById('2_2');
+        var c = document.getElementById('2_3');
+        var d = document.getElementById('2_4');
+        var e = document.getElementById('2_5');
+        var f = document.getElementById('2_6');
+
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        d.style.display = "none";
+        e.style.display = "none";
+        f.style.display = "none";
+    }
+}
+
+function show3() {
+    // Use the identity matrix while clearing the canvas
+    var qn =  $('#ch3').val();
+    if(qn == 'q1') {
+        var a = document.getElementById('3_1');
+        var b = document.getElementById('3_2');
+        var c = document.getElementById('3_3');
+        var d = document.getElementById('3_4');
+        var e = document.getElementById('3_5');
 
         a.style.display = "block";
         b.style.display = "none";
@@ -212,11 +241,11 @@ function show2() {
     }
 
     else if(qn == 'q2') {
-        var a = document.getElementById('2_1');
-        var b = document.getElementById('2_2');
-        var c = document.getElementById('2_3');
-        var d = document.getElementById('2_4');
-        var e = document.getElementById('2_5');
+        var a = document.getElementById('3_1');
+        var b = document.getElementById('3_2');
+        var c = document.getElementById('3_3');
+        var d = document.getElementById('3_4');
+        var e = document.getElementById('3_5');
 
         a.style.display = "none";
         b.style.display = "block";
@@ -226,11 +255,11 @@ function show2() {
     }
 
     else if(qn == 'q3') {
-        var a = document.getElementById('2_1');
-        var b = document.getElementById('2_2');
-        var c = document.getElementById('2_3');
-        var d = document.getElementById('2_4');
-        var e = document.getElementById('2_5');
+        var a = document.getElementById('3_1');
+        var b = document.getElementById('3_2');
+        var c = document.getElementById('3_3');
+        var d = document.getElementById('3_4');
+        var e = document.getElementById('3_5');
 
         a.style.display = "none";
         b.style.display = "none";
@@ -240,11 +269,11 @@ function show2() {
     }
 
     else if(qn == 'q4') {
-        var a = document.getElementById('2_1');
-        var b = document.getElementById('2_2');
-        var c = document.getElementById('2_3');
-        var d = document.getElementById('2_4');
-        var e = document.getElementById('2_5');
+        var a = document.getElementById('3_1');
+        var b = document.getElementById('3_2');
+        var c = document.getElementById('3_3');
+        var d = document.getElementById('3_4');
+        var e = document.getElementById('3_5');
 
         a.style.display = "none";
         b.style.display = "none";
@@ -254,11 +283,11 @@ function show2() {
     }
 
     else if(qn == 'q5') {
-        var a = document.getElementById('2_1');
-        var b = document.getElementById('2_2');
-        var c = document.getElementById('2_3');
-        var d = document.getElementById('2_4');
-        var e = document.getElementById('2_5');
+        var a = document.getElementById('3_1');
+        var b = document.getElementById('3_2');
+        var c = document.getElementById('3_3');
+        var d = document.getElementById('3_4');
+        var e = document.getElementById('3_5');
 
         a.style.display = "none";
         b.style.display = "none";
@@ -268,11 +297,11 @@ function show2() {
     }
 
     else {
-        var a = document.getElementById('2_1');
-        var b = document.getElementById('2_2');
-        var c = document.getElementById('2_3');
-        var d = document.getElementById('2_4');
-        var e = document.getElementById('2_5');
+        var a = document.getElementById('3_1');
+        var b = document.getElementById('3_2');
+        var c = document.getElementById('3_3');
+        var d = document.getElementById('3_4');
+        var e = document.getElementById('3_5');
 
         a.style.display = "none";
         b.style.display = "none";
