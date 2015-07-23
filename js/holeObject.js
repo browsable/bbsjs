af11bf8eb1510effe202a4105ce7e36930cb7d27
hernia <hernia@koreatech.ac.kr>
2015-07-23 22:13:11 +0900

    holeObject 객체 생성중

--- js/holeObject.js
+++ js/holeObject.js
@@ -3,7 +3,327 @@
  */
 var hole0_0 = {
     id: "hole0_0",
-    color: "red",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole0_1 = {
+    id: "hole0_1",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole0_2 = {
+    id: "hole0_2",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole0_3 = {
+    id: "hole0_3",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole0_4 = {
+    id: "hole0_4",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole0_5 = {
+    id: "hole0_5",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole0_6 = {
+    id: "hole0_6",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole0_7 = {
+    id: "hole0_7",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole0_8 = {
+    id: "hole0_8",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole0_9 = {
+    id: "hole0_9",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+
+var hole0_10 = {
+    id: "hole0_10",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+
+var hole0_11 = {
+    id: "hole0_11",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+
+var hole0_12 = {
+    id: "hole0_12",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+
+var hole0_13 = {
+    id: "hole0_13",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+
+var hole0_14 = {
+    id: "hole0_14",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+
+var hole0_15 = {
+    id: "hole0_15",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+
+var hole0_16 = {
+    id: "hole0_16",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+
+var hole0_17 = {
+    id: "hole0_17",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+
+var hole0_18 = {
+    id: "hole0_18",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+
+var hole0_19 = {
+    id: "hole0_19",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+
+var hole0_20 = {
+    id: "hole0_20",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole0_21 = {
+    id: "hole0_21",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole0_22 = {
+    id: "hole0_22",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole0_23 = {
+    id: "hole0_23",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole0_24 = {
+    id: "hole0_24",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+
+var hole1_0 = {
+    id: "hole1_0",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole1_1 = {
+    id: "hole1_1",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole1_2 = {
+    id: "hole1_2",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole1_3 = {
+    id: "hole0_3",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole1_4 = {
+    id: "hole0_4",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole1_5 = {
+    id: "hole1_5",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole1_6 = {
+    id: "hole1_6",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole1_7 = {
+    id: "hole1_7",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole1_8 = {
+    id: "hole1_8",
+    V : 0,
+    I : 0,
+    R : 0,
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+};
+var hole1_9 = {
+    id: "hole1_9",
+    V : 0,
+    I : 0,
+    R : 0,
     getInfo: function () {
         return this.color + ' ' + this.type + ' apple';
     }
